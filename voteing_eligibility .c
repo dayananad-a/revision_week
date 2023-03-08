3092f8a3a00dfd70e2a6fb2916500d49d777096f
dayananad-a <78206511+dayananad-a@users.noreply.github.com>
2023-03-08 08:27:53 +0530

    Create voteing_eligibility .c
    Checks for voting eligibility  and if not eligible display the number of years remaining

---
+++ voteing_eligibility .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+     int age;
+     printf("Enter Age of Person : ");
+     scanf("%d",&age);
+     if(age>=18)
+     {
+     	printf("\nPerson is Eligible for Voting");
+	 }
+     else
+     {
+     	printf("\nPerson is NOT Eligible for Voting more %d years to vote.", 18-age);
+	 }
+}
