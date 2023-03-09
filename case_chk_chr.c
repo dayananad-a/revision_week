819d91aa3133672e9d19cf94fb010baa53c15a6e
dayananad-a <78206511+dayananad-a@users.noreply.github.com>
2023-03-09 08:59:56 +0530

    Create case_chk_chr.c
    Check whether the entered character is uppercase, lowercase and digit using cases.

---
+++ case_chk_chr.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() 
+{
+    char input;
+    printf("Enter a character: ");
+    scanf("%c", &input);
+
+    switch (input) 
+	{
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            printf("The input is a digit.\n");
+            break;
+
+        case 'A':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'I':
+        case 'J':
+        case 'K':
+        case 'L':
+        case 'M':
+        case 'N':
+        case 'O':
+        case 'P':
+        case 'Q':
+        case 'R':
+        case 'S':
+        case 'T':
+        case 'U':
+        case 'V':
+        case 'W':
+        case 'X':
+        case 'Y':
+        case 'Z':
+            printf("The input is an uppercase letter.\n");
+            break;
+
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'i':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'o':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'u':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+            printf("The input is a lowercase letter.\n");
+            break;
+
+        default:
+            printf("The input is a special character.\n");
+            break;
+    }
+
+    return 0;
+}
